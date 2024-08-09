0022ddcf41f3e0efb29e8c5d18b89f2849580658
JO33338 <John.Driscoll@ll.mit.edu>
2024-08-09 13:54:50 -0400

    Begun function to parse dataset

--- main.c
+++ main.c
@@ -7,58 +7,6 @@
 
 #define PATHLENLIM 100
 
-/*
-int test() {
-    // Test day struct
-    char testDate[DATELEN] = "2023-08-30";
-    char **columnNames = malloc(2 * sizeof(char *));
-    char *column1 = malloc(5 * sizeof(char));
-    char *column2 = malloc(5 * sizeof(char));
-    
-    // Build column 1 name
-    column1[0] = 't';
-    column1[1] = 'e';
-    column1[2] = 'm';
-    column1[3] = 'p';
-    column1[4] = '\0';
-    columnNames[0] = column1;
-
-    // Build column 2 name
-    column2[0] = 'w';
-    column2[1] = 'i';
-    column2[2] = 'n';
-    column2[3] = 'd';
-    column2[4] = '\0';
-    columnNames[1] = column2;
-
-    // Build the values
-    float *columnValues = malloc(2 * sizeof(float));
-    columnValues[0] = 77.0;
-    columnValues[1] = 5.0;
-    
-    int *numColumns = malloc(sizeof(int));
-    *numColumns = 2; 
-
-    // Construct the day
-    dayData *d = init_day(testDate, columnNames, columnValues, numColumns);
-
-    // Print 
-    int i = 0;
-    while(d->columnNames[1][i] != '\0') {
-        printf("%c", d->columnNames[1][i]);
-        i++;
-    }
-    printf("\n");
-
-    free(columnNames);
-    free(column1);
-    free(column2); 
-    free(numColumns); 
-    free(d); 
-    return 0;
-}
-*/
-
 // Gather an existing path to the database.
 char *getDataPath() {
     char dataPath[PATHLENLIM]; 
@@ -97,10 +45,26 @@ int getDataSize() {
 }
 
 // Gather the entries from the dataset. 
-void gatherData(char *dataPath, int setSize) {
-    FILE *fptr = fopen(dataPath, 'r');
+dayData *gatherData(char *dataPath, int setSize) {
+    // Setup: file pointer, 
+    FILE *fptr = fopen(dataPath, "r");
+    char *entry;
+    size_t len = 0; 
+    ssize_t line = 0; 
+    int numEntriesAdded = 0; 
+    dayData *dayEntries = malloc(setSize * sizeof(dayData));
 
+    char inQuotes = 0;
+    int i = 0;
+    while((line = getline(&entry, &len, fptr)) != -1 && numEntriesAdded < setSize) {
+        for(i = 0; i < strlen(entry); i++)
+           printf("%c", entry[i]);
+        numEntriesAdded++;
+    }
+
+    free(entry);
     fclose(fptr); 
+    return dayEntries;
 }
 
 int main() {
@@ -112,8 +76,10 @@ int main() {
     int setSize = getDataSize(); 
     printf("Selected dataset size: %d\n", setSize);
 
-    gatherData(dataPath, setSize);
+    // Gather data
+    dayData *dayEntries = gatherData(dataPath, setSize);
 
     free(dataPath); 
+    free(dayEntries); 
     return 0;
 }
